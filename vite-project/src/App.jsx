d6cb5e18092222f1487e78320066d593a3ee8c08
Kauan <162659253+solidsoon@users.noreply.github.com>
2024-05-20 05:54:29 -0300

    Enviando projeto para deploy

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Conversor from './components/Converter'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className='Converter'>
+      <Conversor />
+    </div>
+    
+
+  )
+}
---
+++ vite-project/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Conversor from './components/Converter'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Conversor />
+  </React.StrictMode>
+)
